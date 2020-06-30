8097c23a6f275b74e1db556ddb24686c3f2f02a0
rodrigodasilva <ribeiro.rodrigodasilva>
2020-06-30 19:35:44 -0300

    chore: add Header in layout structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header = () => {
+  return (
+    <Container>
+      <h4>Evolux</h4>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 0px 32px;
+  background: #24292e;
+  color: #fff;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 52px;
+`;
